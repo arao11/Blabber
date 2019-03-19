89ccda3367efa80abf81e6b5c9e9657a16acf2f3
Arjun Rao <arjun.rao217@gmail.com>
2019-03-19 17:18:54 -0400

    Finished POST and GET

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(bodyParser.json());
+
 const PORT = 3000;
 
-const currId = 0;
+var currId = 0;
 
 function BlabModel (id, postTime, author, message) {
   this.id = id;
@@ -14,40 +16,44 @@ function BlabModel (id, postTime, author, message) {
   this.message = message;
 }
 
-const blabArray = [];
+var blabArray = [];
 
-app.listen(PORT, () => {
-  console.log('Server running on port ' + PORT);
+app.get('/blabs', (req, res) => {
+  if (!req.body.createdSince) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'createdSince is required'
+    });
+  }
+  var i;
+  for (i = 0; i < currId; i++) {
+    if (blabArray[i].postTime >= req.body.createdSince) {
+      res.status(200).send(blabArray.slice(i));
+    }
+  }
 });
-
-// app.get('/blabs', (req, res) => {
-//   console.log("get");
-//   res.status(200).send({
-//     success: 'true',
-//     message: 'todos retrieved successfully',
-//   })
-// });
 
 
 app.post('/blabs', (req, res) => {
-  console.log("post");
-  res.status(200).send({
-    success: 'true',
-    message: 'todos retrieved successfully',
-  })
-  // if (!req.body.author) {
-  //   return res.status(400).send({
-  //     success: 'false',
-  //     message: 'Author is required'
-  //   });
-  // }
-  // if (!req.body.message) {
-  //   return res.status(400).send({
-  //     success: 'false',
-  //     message: 'Message is required'
-  //   });
-  // }
-  // const blab = new BlabModel(currId, Date.now(), req.body.author, req.body.message);
-  // blabArray[currId] = blab;
-  // currId++;
+  //console.log("post");
+  if (!req.body.author) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'Author is required'
+    });
+  }
+  if (!req.body.message) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'Message is required'
+    });
+  }
+  const blab = new BlabModel(currId, Date.now(), req.body.author, req.body.message);
+  blabArray[currId] = blab;
+  currId++;
+  res.status(201).send(blab);
+});
+
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
 });
