408407de7bb3bd07cecd1fd8af58f7b060513ab8
Arjun Rao <arjun.rao217@gmail.com>
2019-04-17 11:45:54 -0400

    get/post/delete work with mongo finally

--- app.js
+++ app.js
@@ -40,24 +40,27 @@ function BlabModel (id, postTime, author, message) {
 
 app.get('/blabs', (req, res) => {
   //console.log('get');
-  var time = req.query.createdSince;
-  if (!time) {
-    time = 0;
+  var time = 0;
+  if (req.query.createdSince) {
+    time = parseInt(req.query.createdSince);
   }
   var query = {postTime: { $gte: time}};
 
-  MongoClient.connect("mongodb://mongo:27017", { useNewUrlParser: true }, (err, database) => {
-    //console.log('connected');
+  db.collection("blabber").find(query).toArray((err, result) => {
     assert.equal(null, err);
-    database.collection("blabber").find(query).toArray((err, result) => {
-      assert.equal(null, err);
-      res.status(200).send(result);
-    })
-  
-    //if (ret == null) {
-      //res.status(200).send([]);
-    //}
-  });
+    res.status(200).send(result);
+  })
+
+  // Change time to int (cast with parseint) DONE
+
+  // make a new docker compose for tests without volumes
+  // fix 404 error for delete, call get before and if doesnt exist, return 404
+
+  // MongoClient.connect("mongodb://mongo:27017", { useNewUrlParser: true }, (err, database) => {
+  //   //console.log('connected');
+  //   assert.equal(null, err);
+    
+  // });
 });
 
 
@@ -92,13 +95,13 @@ app.post('/blabs', (req, res) => {
 app.delete('/blabs/:id', (req, res) => {
   var currId = req.params.id.toString();
   var query = {id: currId};
-  var hasDeleted = false;
-  db.collection("blabber").deleteOne(query, function(err, docs) {
+  db.collection("blabber").find(query, (err, result) => {
+    if (result == null) {
+      res.status(404).send("Blab not found");
+    }
+  });
+  db.collection("blabber").deleteOne(query, (err, result) => {
     assert.equal(null, err);
-    hasDelete = true;
     res.status(200).send("Blab deleted Successfully");
   });
-  if (!hasDeleted) {
-    res.status(404).send("Blab not found");
-  }
 });
