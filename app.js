a01a73c953e3eb2129691873ad847433e427721a
Arjun Rao <arjun.rao217@gmail.com>
2019-05-08 15:27:45 -0400

    Cleaned up code

--- app.js
+++ app.js
@@ -23,17 +23,13 @@ var db;
 var username = process.env.mongoUSR;
 var password = fs.readFileSync(process.env.mongoPWD).toString();
 
-//console.log('password: ' + password);
-
 const url = encodeURI(`mongodb://${username}:${password}@mongo:27017`)
 
 MongoClient.connect(url, { useNewUrlParser: true }, (err, database) => {
-  //console.log('connected');
   assert.equal(null, err);
   db = database.db("blabber");
 
   db.createCollection('blabber', function(err, collection) {});
-  //console.log(db);
 
   app.listen(PORT, () => {
     console.log('Now listening on port ' + PORT);
@@ -54,23 +50,15 @@ app.get('/blabs', (req, res) => {
     time = parseInt(req.query.createdSince);
   }
   var query = {postTime: { $gte: time}};
-  //console.log("using query", query);
 
   db.collection("blabber").find(query).toArray((err, result) => {
     assert.equal(null, err);
     res.status(200).send(result);
   })
-
-  // Change time to int (cast with parseint) DONE
-
-  // make a new docker compose for tests without volumes
-  // fix 404 error for delete, call get before and if doesnt exist, return 404
-
 });
 
 
 app.post('/blabs', (req, res) => {
-  // console.log('post');
   if (!req.body.author) {
     return res.status(400).send({
       success: 'false',
@@ -89,11 +77,7 @@ app.post('/blabs', (req, res) => {
   db.collection("blabber").insertOne(blab, (err, result) => {
     blabCounter.inc();
     assert.equal(null, err);
-    //console.log(blab);
     res.status(201).send(blab);
-    // setTimeout(() => {
-    //   res.status(201).send(blab);
-    // }, 2000);
   });
 });
 
@@ -121,21 +105,8 @@ var collectDefaultMetrics = Prometheus.collectDefaultMetrics;
 collectDefaultMetrics({ timeout: 5000 });
 
 app.get('/metrics', (req, res) => {
-  //res.status(200).send(Prometheus.register.metrics())
-  const end = blabCounter.startTimer();
   res.set('Content-Type', Prometheus.register.contentType)
   res.end(Prometheus.register.metrics())
-  end();
-
-  // httpRequestDurationMicroseconds
-  // .labels(req.route.path)
-  // .observe(responseTimeInMs)
-
-  // blabCounter
-  // .labels(req.route.path)
-  // .observe(responseTimeInMs)
-
-  
 })
 
 var httpRequestDurationMicroseconds = new Prometheus.Histogram({
